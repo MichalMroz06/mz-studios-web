fc33f61ac461c0e4a59b32af73d8e15cfdd138ed
Michał Mróz <michalmroz2006@gmail.com>
2026-08-27 11:27:15 +0200

    Update files to project structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
 import Container from '@mui/material/Container';
-import HeroSection from '@/components/HeroSection';
-import GamesSection from '@/components/GamesSection';
-import GameTechSection from '@/components/GameTechSection';
-import ContactBanner from '@/components/ContactBanner';
+import HeroSection from '@/components/main-page/HeroSection';
+import GamesSection from '@/components/main-page/GamesSection';
+import GameTechSection from '@/components/main-page/GameTechSection';
+import ContactBanner from '@/components/main-page/ContactBanner';
 
 export default function Home() {
   return (
